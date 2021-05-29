878447dea23c469d9d0f27758635fbe18a82ff1c
thienho <thienho1998dragon@gmail.com>
2021-05-29 18:55:27 +0700

    8 test case (3 internal 5 normal)

---
+++ CDNC_Hw2_Project/CDNC_Hw2_Katalon_Project/Object Repository/Popup New Chanel/Header TextArea.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Header TextArea</name>
+   <tag></tag>
+   <elementGuidId>091eae53-fc7a-47df-a1be-4a2c48835e6c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'newChannelHeader' and @class = 'form-control no-resize']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>newChannelHeader</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control no-resize</value>
+   </webElementProperties>
+</WebElementEntity>
